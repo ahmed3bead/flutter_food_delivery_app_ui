d9b749c3e7277ed5fb2a3e173ba543275ab697ac
unknown <ahmedm3bead@hotmail.com>
2020-05-27 16:23:19 +0200

    first commit

---
+++ android/app/src/main/java/com/aecode/flutterfooddeliveryappui/MainActivity.java
@@ -0,0 +1,6 @@
+package com.aecode.flutterfooddeliveryappui;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+
+import 'package:flutterfooddeliveryappui/core/consts.dart';
+import 'package:flutterfooddeliveryappui/core/flutter_icons.dart';
+import 'package:flutterfooddeliveryappui/models/food_model.dart';
+import 'package:flutterfooddeliveryappui/widgets/app_clipper.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'dart:math' as math;
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
